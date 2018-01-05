67f6a1b5e1721f3b456408276064c21b55bcf7bd
amulya-pokala1 <34930151+amulya-pokala1@users.noreply.github.com>
2018-01-05 22:56:37 +0530

    Add files via upload

---
+++ jdemo/src/main/java/Demo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+	List l=new ArrayList();
+	public List getData()
+	{
+		
+		l.add("hey");
+		l.add("hello");
+		l.add("berry");
+		return l;
+	}
+
+}
